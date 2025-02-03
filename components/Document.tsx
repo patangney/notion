843edeb52d544789558a13118a9a399a4c97ae88
Patrick Tangney <ptangney@outlook.com>
2025-02-03 16:11:34 +0000

    create update function for update title

--- components/Document.tsx
+++ components/Document.tsx
@@ -1,9 +1,63 @@
 'use client';
 
+import { useEffect, useState, useTransition } from 'react';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import { db } from '@/firebase';
+import { doc, updateDoc } from 'firebase/firestore';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+
 function Document({ id: _id }: { id: string }) {
+  const [data, loading, error] = useDocumentData(doc(db, 'documents', _id));
+  const [input, setInput] = useState(_id);
+  const [isUpdating, startTransition] = useTransition();
+
+  useEffect(() => {
+    // fetch document
+    if (loading) {
+      console.log('Loading data...');
+      return;
+    }
+    if (error) {
+      console.error('Error fetching data:', error);
+      return;
+    }
+    if (!data) {
+      console.log('No data found');
+      return;
+    }
+    if (data) {
+      setInput(data.title);
+    }
+  }, [data, loading, error]);
+
+  const updateTitle = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (input.trim()) {
+      startTransition(async () => {
+        await updateDoc(doc(db, 'documents', _id), { title: input });
+        // update document
+      });
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      updateTitle(e as unknown as React.FormEvent);
+    }
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={updateTitle}>
+        <Input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <Button disabled={isUpdating} type="submit">
+          {isUpdating ? 'Updating...' : 'Update'}
+        </Button>
         {/* update title... */}
         {/* isOwner && inviteUser, DeleteDocument */}
       </form>
@@ -11,7 +65,7 @@ function Document({ id: _id }: { id: string }) {
         {/* Manage Users */}
         {/* Avatars */}
       </div>
-      {/* Collaborative Editro */}
+      {/* Collaborative Editor */}
     </div>
   );
 }
